39c35089aa42c2abe093d23ab66b2a666bfcd7bf
Ayush <ayushagarwal.dev@gmail.com>
2025-03-20 17:33:59 +0530

    use stdouput for print and stderr for error

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1)
     });
 
@@ -12,6 +13,7 @@ fn main() {
     println!("In file {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
